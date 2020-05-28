a62fe76bf525330b2a73152803256365829ba6e6
sahilp-tech <patelsahil395@gmail.com>
2020-05-28 16:23:55 -0700

    First Commit

---
+++ functions/dbschema.js
@@ -0,0 +1,12 @@
+//How our data will look like; serves as a reference. File is not used
+let db = {
+    takes: [
+        {
+            userHandle: 'user',
+            body: 'take body',
+            createdAt: '2011-10-05T14:48:00.000Z',
+            likeCount: 6, //how many likes a take has
+            commentCount: 2 //how many comment a take has
+        }
+    ]
+}
